b4a233c212db4c3d79c92c8f12a22c30b533c8e9
Mirtha Jimenez <mirtha.jimenez.leon@gmail.com>
2023-05-07 20:49:07 -0400

    added mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetHelloWorld.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
